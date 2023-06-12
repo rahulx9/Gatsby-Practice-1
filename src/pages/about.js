ed53a6f17a69492c0843f9c8b5df1617d6da9636
rahulx9 <74450641+rahulx9@users.noreply.github.com>
2023-06-12 16:57:27 +0530

    Added few pages and styles

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <div>About</div>
+      <p>
+        Cillum irure duis qui exercitation quis aliquip quis voluptate Lorem
+        duis velit velit. Ad amet commodo labore ex nisi consequat occaecat
+        deserunt consectetur proident excepteur quis. Minim proident sit cillum
+        ex excepteur. Ex proident consequat eiusmod cupidatat eiusmod. Sint
+        tempor pariatur id pariatur sunt cupidatat qui deserunt. Minim mollit
+        quis aliqua dolor sint cillum quis id quis ad deserunt qui. Ex elit ut
+        dolore ullamco est culpa ut excepteur. Deserunt dolor fugiat commodo qui
+        minim sunt non qui labore dolore aliquip Lorem incididunt sint.
+        Incididunt irure consequat occaecat nulla aliquip exercitation amet non
+        ad eu.
+      </p>
+    </Layout>
+  );
+}
